8404ce0a87844574c90b382a5018c406d8c2be8b
mateo-a <alexander.urrego@gmail.com>
2020-07-14 17:48:15 -0500

    [Add] Task 4

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 0; n <= 98; n++)
+                Console.WriteLine("{0} = 0x{0:x}", n);
+        }
+    }
+}

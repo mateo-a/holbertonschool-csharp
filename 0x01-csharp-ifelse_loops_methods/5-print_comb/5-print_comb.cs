91b03a341640061358c0862204ea4d6d8b7a77ec
mateo-a <alexander.urrego@gmail.com>
2020-07-14 18:02:07 -0500

    [Add] Task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 0; n <= 99; n++)
+                if (n == 99)
+                    Console.Write("{0:D2}\n", n);
+                else
+                    Console.Write("{0:D2}, ", n);
+        }
+    }
+}

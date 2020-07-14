baeefee7bafb1f806608c23197e7f797b3499fca
mateo-a <alexander.urrego@gmail.com>
2020-07-14 16:58:14 -0500

    [Fix] Task 0

--- 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -6,12 +6,11 @@ class Program
     {
         Random rndm = new Random();
         int number = rndm.Next(-10, 10);
-        if (number > 0) {
+        if (number > 0)
             Console.WriteLine("{0} is positive", number);
-        } else if (number < 0){
+        else if (number < 0)
             Console.WriteLine("{0} is negative", number);
-        } else {
+        else
             Console.WriteLine("{0} is zero", number);
-        }
     }
 }

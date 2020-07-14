6160b891e1c860d1b62459d0e05f46374d78d233
mateo-a <alexander.urrego@gmail.com>
2020-07-14 18:39:54 -0500

    [Add] Task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0; x <= 9; x++)
+                for (int y = (x + 1); y <= 9; y++)
+                if (x == 8 && y == 9)
+                    Console.Write("{0}{1}", x, y);
+                else
+                    Console.Write("{0}{1}, ", x, y);
+        }
+    }
+}

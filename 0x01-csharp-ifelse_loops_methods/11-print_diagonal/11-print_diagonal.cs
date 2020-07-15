28d99cb1b359fba84e0a136b2aca7dad3c8f6a96
mateo-a <alexander.urrego@gmail.com>
2020-07-14 20:10:17 -0500

    [Add] Task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+            Console.WriteLine("\n");
+        else
+        {
+            for (int i = 1; i <= length; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write(" ");
+                Console.WriteLine("\\");
+            }
+        }
+    }
+}

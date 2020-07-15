2aa1c5a8e0d7752718e2f9545a2a8cec8aec084f
mateo-a <alexander.urrego@gmail.com>
2020-07-14 19:32:13 -0500

    [Add] Task 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int i = number % 10;
+        if (i < 0)
+            i = i * -1;
+        Console.Write(i);
+        return i;
+    }
+}

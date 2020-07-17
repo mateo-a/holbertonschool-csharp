9212b14944348e5c16b9b158e51b1ac8db7a4160
mateo-a <alexander.urrego@gmail.com>
2020-07-16 23:39:12 -0500

    [Add] Task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = new int[5, 5];
+        array[2, 2] = 1;
+        for (int col = 0; col < 5; col++)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                if (row > 0)
+                    Console.Write(" ");
+                Console.Write(array[col, row]);
+            }
+            Console.WriteLine("");
+        }
+    }
+}

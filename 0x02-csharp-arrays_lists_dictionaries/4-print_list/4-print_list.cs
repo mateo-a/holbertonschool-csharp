eef2f816c9977ddfd74fda49377c91a37d2ebc43
mateo-a <alexander.urrego@gmail.com>
2020-07-16 21:29:58 -0500

    [Add] Task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List <int> lis = new List<int>();
+        for (int val = 0; val < size; val++){
+            lis.Add(val);
+            if (val > 0)
+                Console.Write(" ");
+            Console.Write(val);
+        }
+        Console.WriteLine("");
+        return lis;
+    }
+}

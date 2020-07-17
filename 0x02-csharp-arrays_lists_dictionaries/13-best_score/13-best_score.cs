f9322877cbb97c73f012d390c5364f1a04d05c29
mateo-a <alexander.urrego@gmail.com>
2020-07-16 23:25:11 -0500

    [Add] Task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int num = 0;
+        string val = "None";
+
+        foreach (KeyValuePair<string, int> key in myList)
+        {
+            if (key.Value >= num)
+            {
+                num = key.Value;
+                val = key.Key;
+            }
+        }
+        return val;
+    }
+}

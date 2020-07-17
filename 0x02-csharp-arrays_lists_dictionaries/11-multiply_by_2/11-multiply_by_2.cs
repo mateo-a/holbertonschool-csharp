ba68c7470b1efb8f9363f76846f5ac6ccfd4dcb4
mateo-a <alexander.urrego@gmail.com>
2020-07-16 23:01:24 -0500

    [Add] Task 10

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> dic = new Dictionary<string, int>();
+        foreach (string val in myDict.Keys)
+            dic[val] = myDict[val] * 2;
+        return dic;
+    }
+}

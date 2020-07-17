052364a1ff824a12da48add2294893742c605eff
mateo-a <alexander.urrego@gmail.com>
2020-07-16 20:00:38 -0500

    [Add] Task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] num = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            num[i] = i;
+            if (i != 0)
+                Console.Write(" ");
+            Console.Write("{0}", i);
+        }
+        Console.WriteLine("");
+        return num;
+    }
+}

23e7a8617f80b364a3de21e78eb589b414340cee
mateo-a <alexander.urrego@gmail.com>
2020-07-16 21:13:24 -0500

    [Fix] Task 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array != null)
+        {
+            for (int val = array.Length - 1; val >= 0; val--)
+            {
+                if (val < array.Length - 1)
+                    Console.Write(" ");
+                Console.Write(array[val]);
+
+            }
+        }
+        Console.WriteLine();
+    }
+}

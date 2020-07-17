97aad3a6419e17f8ca1da82020709de0d98ffcda
mateo-a <alexander.urrego@gmail.com>
2020-07-16 21:56:21 -0500

    [Add] Task 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> lis = new List<bool>();
+        foreach (int val in myList)
+        {
+            if (val % 2 == 0)
+                lis.Add(true);
+            else
+                lis.Add(false);
+        }
+        return lis;
+    }
+}

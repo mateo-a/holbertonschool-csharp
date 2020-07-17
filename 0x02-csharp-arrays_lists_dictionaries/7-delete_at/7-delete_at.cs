bc9663ba70ea969d8960406183c8152f3673415d
mateo-a <alexander.urrego@gmail.com>
2020-07-16 22:17:58 -0500

    [Add] Task 7

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -5,9 +5,9 @@ class List
 {
     public static List<int> DeleteAt(List<int> myList, int index)
     {
-        if (index < 0 || index > myList.Count -1)
+        if (index < 0 || index > myList.Count - 1)
             Console.WriteLine("Index is out of range");
-        else
-            myList.Remove(myList[index]);
+        myList.Remove(myList[index]);
+        return myList;
     }
 }

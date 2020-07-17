401f5ecd7f873425dbb95a2c016a43e58ebfc403
mateo-a <alexander.urrego@gmail.com>
2020-07-17 00:15:00 -0500

    [Add] Task 100

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] array = new int[3][];
+            array[0] = new int[4] { 0, 1, 2, 3 };
+            array[1] = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+            array[2] = new int[2] { 0, 1 };
+
+            for (int col = 0; col < array.Length; col++)
+            {
+                for (int row = 0; row < array[col].Length; row++)
+                {
+                    if (row > 0)
+                        Console.Write(" ");
+                    Console.Write(array[col][row]);
+                }
+            }
+        }
+    }
+}

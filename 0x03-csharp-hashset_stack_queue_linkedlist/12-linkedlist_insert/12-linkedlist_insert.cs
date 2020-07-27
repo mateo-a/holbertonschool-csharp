3234892237eafd8f81aac304cb1bc4933e441334
mateo-a <alexander.urrego@gmail.com>
2020-07-27 16:31:59 -0500

    [ADD] Tasks 11-13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> index = myLList.First;
+
+        while (index != null)
+        {
+            if (n < index.Value)
+                return myLList.AddBefore(index, n);
+            index = index.Next;
+        }
+        return myLList.AddLast(n);
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int i;
+
+        LinkedListNode<int> node = myLList.First;
+        for (i = 0; i < index && node.Next != null; i++)
+            node = node.Next;
+        if (i == index)
+            myLList.Remove(node);
+    }
+}

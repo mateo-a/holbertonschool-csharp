240c07d113fbf18e2dc7ea7c694055e57de5d013
mateo-a <alexander.urrego@gmail.com>
2020-07-27 12:54:11 -0500

    [ADD] Tasks 1-2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> temp1 = new HashSet<int>(list1);
+        HashSet<int> temp2 = new HashSet<int>(list2);
+
+        temp1.IntersectWith(temp2);
+
+        List<int> newList = new List<int>(temp1);
+
+        return newList;
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> tempList = new List<int>();
+        foreach (int num in list1)
+        {
+            if (!list2.Contains(num))
+                tempList.Add(num);
+            else
+                continue;
+        }
+        foreach (int num in list2)
+        {
+            if (!list1.Contains(num))
+                tempList.Add(num);
+            else
+                continue;
+        }
+        tempList.Sort();
+        return (tempList);
+    }
+}

cb56f16a54f162f004f0ccc1d6a2fc86f88978a6
mateo-a <alexander.urrego@gmail.com>
2020-07-27 12:33:59 -0500

    [ADD] Task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        HashSet<int> tempList = new HashSet<int>(myList);
+
+        foreach (var num in tempList)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+}

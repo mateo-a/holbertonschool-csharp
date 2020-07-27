c77a558ca11e359a3fde9a8102c73fb29324742e
mateo-a <alexander.urrego@gmail.com>
2020-07-27 13:22:41 -0500

    [ADD] Tasks 3-4

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        bool check = aStack.Contains(search);
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        Console.WriteLine($"Stack contains \"{search}\": {check}");
+        if (check)
+        {
+            while (aStack.Pop() != search);
+        }
+        aStack.Push(newItem);
+        return (aStack);
+    }
+}
---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        bool check = aQueue.Contains(search);
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine($"First item: {aQueue.Peek()}");
+        Console.WriteLine($"Queue contains \"{search}\": {check}");
+        if (check)
+        {
+            while (aQueue.Dequeue() != search);
+        }
+        aQueue.Enqueue(newItem);
+        return (aQueue);
+    }
+}

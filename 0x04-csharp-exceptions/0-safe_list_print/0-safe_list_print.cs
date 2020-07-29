14a129424d75541b3086ef1491cca38cd9d8e938
mateo-a <alexander.urrego@gmail.com>
2020-07-28 19:48:42 -0500

    [ADD] Task 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int elements = 0;
+        try
+        {
+            for (elements = 0; elements < n; elements++)
+                Console.WriteLine(myList[elements]);
+        }
+        catch (System.Exception)
+        {
+            return (elements);
+        }
+        return (elements);
+    }
+}

b8c9cf414118bc329d6a8fd1feebb01ff0f980fa
mateo-a <alexander.urrego@gmail.com>
2020-08-04 22:40:53 -0500

    [Add] Task 3

---
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void IsPalindrome_Symbols()
+        {
+            string test = "!?. :;";
+            Assert.That(Text.Str.IsPalindrome(test), Is.True);
+        }
+        [Test]
+        public void IsPalindrome_Panama()
+        {
+            string test = "A man, a plan, a canal: Panama.";
+            Assert.That(Text.Str.IsPalindrome(test), Is.True);
+        }
+        [Test]
+        public void IsPalindrome_Abaj()
+        {
+            string test = "ABAJABAJ";
+            Assert.That(Text.Str.IsPalindrome(test), Is.False);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Text
+{
+    /// <summary>
+    /// Str Class
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// Method that returns True if a string is a palindrome or False if it’s not.
+        /// </summary>
+        /// <param name="s">A string to be checked</param>
+        /// <returns></returns>
+        public static bool IsPalindrome(string s)
+        {
+            List<char> str = new List<char>();
+            foreach (char val in s)
+                if (!Char.IsPunctuation(val) && !Char.IsWhiteSpace(val))
+                    str.Add(Char.ToLower(val));
+            List<char> reversed = new List<char>(str);
+            reversed.Reverse();
+            return (str.SequenceEqual(reversed));
+        }
+    }
+}

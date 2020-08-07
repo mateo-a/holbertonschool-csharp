f5f78796129a37d60d1ef6c60618822e81c29b71
mateo-a <alexander.urrego@gmail.com>
2020-08-06 21:20:17 -0500

    [Add] Task 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+
+///<summary>Checks types.</summary>
+class Obj
+{
+    ///<summary>Prints properties/methods of an object.</summary>
+    public static void Print(object myObj)
+    {
+        TypeInfo obj = myObj.GetType().GetTypeInfo();
+        Console.WriteLine($"{obj.Name} Properties:");
+        foreach (PropertyInfo val in obj.GetProperties())
+        {
+            Console.WriteLine($"{val.Name}");
+        }
+        Console.WriteLine($"{obj.Name} Methods:");
+        foreach (MethodInfo val in obj.GetMethods())
+        {
+            Console.WriteLine($"{val.Name}");
+        }
+    }
+}

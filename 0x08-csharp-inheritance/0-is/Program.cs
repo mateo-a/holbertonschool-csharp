8367fd22f7ecc30610c6f1f06a8568278f07c2a6
mateo-a <alexander.urrego@gmail.com>
2020-08-06 20:10:59 -0500

    [Add] Task 0

---
+++ 0x08-csharp-inheritance/0-is/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Obj Class
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Method that returns True if the object is an int, otherwise return False.
+    /// </summary>
+    /// <param name="obj">Object to be checked</param>
+    /// <returns>True if object is an int, otherwise return False.</returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return (obj is int);
+    }
+}

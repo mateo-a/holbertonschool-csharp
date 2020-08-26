e774df1c3ff3faa8e1e829bebfff8c5bb6b3b4c4
mateo-a <alexander.urrego@gmail.com>
2020-08-26 17:31:21 -0500

    [Fix] Task 6

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -14,11 +14,11 @@ class VectorMath
         {
             if (vector1.Length == 2)
             {
-                vector3 = new double[2] { vector1[0] + vector2[0], vector1[1] + vector2[1] };
+                vectorRes = new double[2] { vector1[0] + vector2[0], vector1[1] + vector2[1] };
             }
             if (vector1.Length == 3)
             {
-                vector3 = new double[3] { vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2] };
+                vectorRes = new double[3] { vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2] };
             }
             return (vectorRes);
         }

e81a31bdc4d97f753b0ea51528c2d39c19785ffa
mateo-a <alexander.urrego@gmail.com>
2020-08-26 17:28:09 -0500

    [Add] Task 6

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+///<summary>Vector addition</summary>
+class VectorMath
+{
+    /// <summary>
+    /// Method that adds two vectors and returns the resulting vector.
+    /// </summary>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double[] vectorRes = new double[1] { -1 };
+
+        if (vector1.Length == vector2.Length)
+        {
+            if (vector1.Length == 2)
+            {
+                vector3 = new double[2] { vector1[0] + vector2[0], vector1[1] + vector2[1] };
+            }
+            if (vector1.Length == 3)
+            {
+                vector3 = new double[3] { vector1[0] + vector2[0], vector1[1] + vector2[1], vector1[2] + vector2[2] };
+            }
+            return (vectorRes);
+        }
+        return (vectorRes);
+    }
+}

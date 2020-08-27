bf24eb9ecbedb7fdfec8fc82fb245224a4aa58a4
mateo-a <alexander.urrego@gmail.com>
2020-08-26 21:03:59 -0500

    [Fix] Task 14

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -6,25 +6,22 @@ class MatrixMath
     /// <summary>method that adds two matrices and returns the resulting matrix.</summary>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        double[,] matrixRes = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+        double[,] matrixRes;
         double[,] matrixError = new double[,] { { -1 } };
 
-
-        if ((matrix1.GetLength(0) == matrix2.GetLength(0)) && (matrix1.GetLength(1) == matrix2.GetLength(1)))
+        if ((matrix1.GetLength(0) == matrix2.GetLength(0)) && (matrix1.GetLength(1) == matrix2.GetLength(1)) && (matrix1.GetLength(0) == 2) && (matrix1.GetLength(1) == 2))
+            matrixRes = new double[2, 2];
+        else if ((matrix1.GetLength(0) == matrix2.GetLength(0)) && (matrix1.GetLength(1) == matrix2.GetLength(1)) && (matrix1.GetLength(0) == 3) && (matrix1.GetLength(1) == 3))
+            matrixRes = new double[3, 3];
+        else
+            return (matrixError);
+        for (int i = 0; i < matrix1.GetLength(0); i++)
         {
-            if ((matrix1.GetLength(1) == 2 && matrix2.GetLength(1) == 2) || (matrix1.GetLength(1) == 3 && matrix2.GetLength(1) == 3))
+            for (int j = 0; j < matrix1.GetLength(1); j++)
             {
-                for (int i = 0; i < matrix1.GetLength(0); i++)
-                {
-                    for (int j = 0; j < matrix1.GetLength(1); j++)
-                    {
-                        matrixRes[i, j] = matrix1[i, j] + matrix2[i, j];
-                    }
-                }
-                return matrixRes;
+                matrixRes[i, j] = matrix1[i, j] + matrix2[i, j];
             }
-            return matrixError;
         }
-        return matrixError;
+        return matrixRes;
     }
 }

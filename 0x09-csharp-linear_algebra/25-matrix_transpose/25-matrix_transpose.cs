7b987b78a06638cb1885dd236f71ffa929c9d6e9
mateo-a <alexander.urrego@gmail.com>
2020-08-26 22:29:34 -0500

    [Add] Task 23

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+///<summary> Transpose matrix</summary>
+class MatrixMath
+{
+    ///<summary>method to transpose a matrix and return the resulting matrix.</summary>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] matrixRes = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                matrixRes[i, j] = matrix[j, i];
+            }
+        }
+        return (matrixRes);
+    }
+}

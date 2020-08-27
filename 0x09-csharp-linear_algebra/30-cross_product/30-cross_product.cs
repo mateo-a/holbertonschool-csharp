28f09085f49cdd5325491de0ef2cc4f312ed48de
mateo-a <alexander.urrego@gmail.com>
2020-08-26 23:01:53 -0500

    [Add] Task 28

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+///<summary>Cross product</summary>
+class VectorMath
+{
+    ///<summary>method that calculates the cross product of two 3D vectors and returns the resulting vector.</summary>
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        double[] vectorRes;
+
+        if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            vectorRes = new double[3] {(vector1[1] * vector2[2]) - (vector1[2] * vector2[1]),
+                                        (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]),
+                                        (vector1[0] * vector2[1]) - (vector1[1] * vector2[0])
+            };
+            return (vectorRes);
+        }
+        return (new double[] { -1 });
+    }
+}

75a93ddf74b495b604adc35f049f998fecb6ecd7
mateo-a <alexander.urrego@gmail.com>
2020-08-26 22:53:53 -0500

    [Fix] Task 26

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -12,15 +12,15 @@ class MatrixMath
             determinant = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
         else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
         {
-            determinant = Math.Round((matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) +
-                                    (matrix[1, 0] * matrix[2, 1] * matrix[0, 2]) +
-                                    (matrix[2, 0] * matrix[0, 1] * matrix[1, 2]) -
-                                    (matrix[0, 0] * matrix[2, 1] * matrix[1, 2]) -
-                                    (matrix[2, 0] * matrix[1, 1] * matrix[0, 2]) -
-                                    (matrix[1, 0] * matrix[0, 1] * matrix[2, 2]), 2);
+            determinant = (matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) +
+                        (matrix[1, 0] * matrix[2, 1] * matrix[0, 2]) +
+                        (matrix[2, 0] * matrix[0, 1] * matrix[1, 2]) -
+                        (matrix[0, 0] * matrix[2, 1] * matrix[1, 2]) -
+                        (matrix[2, 0] * matrix[1, 1] * matrix[0, 2]) -
+                        (matrix[1, 0] * matrix[0, 1] * matrix[2, 2]);
         }
         else
             return -1;
-        return (determinant);
+        return (Math.Round(determinant, 2));
     }
 }

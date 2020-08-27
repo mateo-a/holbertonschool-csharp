428e8a1f27c721a2be3b8183c330e6f7793c0655
mateo-a <alexander.urrego@gmail.com>
2020-08-26 21:27:57 -0500

    [Add] Task 18

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+///<summary>Matrix-matrix multiplication</summary>
+class MatrixMath
+{
+    ///<summary>method that multiplies two matrices and returns the resulting matrix.</summary>
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] matrixRes;
+        double[,] matrixError = new double[,] { { -1 } };
+        double res;
+
+        if (matrix1.GetLength(1) == matrix2.GetLength(0))
+            matrixRes = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+        else
+            return (matrixError);
+        for (int i = 0; i < matrixRes.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrixRes.GetLength(1); j++)
+            {
+                res = 0;
+                for (int k = 0; k < match; k++)
+                {
+                    res += matrix1[i, k] * matrix2[k, j];
+                }
+                matrixRes[i, j] = res;
+            }
+        }
+        return (matrixRes);
+    }
+}

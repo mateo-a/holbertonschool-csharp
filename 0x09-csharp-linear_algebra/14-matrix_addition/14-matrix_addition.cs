b8913082a4d5c72b5a2f4f378018565d04a8e90a
mateo-a <alexander.urrego@gmail.com>
2020-08-26 20:50:08 -0500

    [Fix] Task 14

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -14,7 +14,7 @@ class MatrixMath
         {
             if ((matrix1.GetLength(1) == 2 && matrix2.GetLength(1) == 2) || (matrix1.GetLength(1) == 3 && matrix2.GetLength(1) == 3))
             {
-                for (int i = 0; i < matrix1.GetLength(1); i++)
+                for (int i = 0; i < matrix1.GetLength(0); i++)
                 {
                     for (int j = 0; j < matrix1.GetLength(1); j++)
                     {

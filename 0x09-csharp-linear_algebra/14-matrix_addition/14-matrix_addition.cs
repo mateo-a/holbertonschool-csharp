fdb09056f59bfc6a200a3045aace959d511ea774
mateo-a <alexander.urrego@gmail.com>
2020-08-26 20:23:51 -0500

    [Add] Task 14

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>Matrix addition</summary>
+class MatrixMath
+{
+    /// <summary>method that adds two matrices and returns the resulting matrix.</summary>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] matrixRes = new double[matrix1.GetLength(1), matrix1.GetLength(1)];
+
+        if ((matrix1.GetLength(1) == 2 && matrix2.GetLength(1) == 2) || (matrix1.GetLength(1) == 3 && matrix2.GetLength(1) == 3))
+        {
+            for (int i = 0; i < matrix1.GetLength(1); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    matrixRes[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+            return matrixRes;
+        }
+        return (-1);
+    }
+}

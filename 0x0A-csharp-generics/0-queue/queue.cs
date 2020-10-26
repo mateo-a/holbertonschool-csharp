9178d772817ccba93c2ac32d5468e442f803a8de
mateo-a <alexander.urrego@gmail.com>
2020-10-26 18:03:56 -0500

    [Add] Task 0

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,10 @@
+﻿
+///<summary>New class called Queue/<T/></summary>
+public class Queue<T>
+{
+    ///<summary>New method CheckType() that returns the Queue’s type.</summary>
+    public string CheckType()
+    {
+        return (typeof(T).ToString());
+    }
+}

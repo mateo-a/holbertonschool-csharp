0fe6db05ff7018c98684051818732d044c1cf334
mateo-a <alexander.urrego@gmail.com>
2020-10-27 18:30:01 -0500

    [Add] Task 0

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+///<summary>Abstract Base class</summary>
+public abstract class Base
+{
+    ///<summary>public property</summary>
+    public string name;
+
+    ///<summary>Override ToString() method</summary>
+    public override string ToString()
+    {
+        return (name + " is a " + this.GetType().ToString());
+    }
+}

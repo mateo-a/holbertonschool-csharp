8e3bda3f0f987e80f1eae36bd6e6e51023381bfa
mateo-a <alexander.urrego@gmail.com>
2020-10-28 19:06:26 -0500

    [Add] Task 1

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+/// <summary>Delegate CalculateHealth that takes a float amount</summary>
+public delegate void CalculateHealth(float amount);
+
+/// <summary>Public class called Player</summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>Constructor</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp > 0)
+        {
+            this.maxHp = maxHp;
+        }
+        else
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.hp = this.maxHp;
+    }
+    /// <summary>Method PrintHealth</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+/// <summary>TakeDamage method that follow the prototype of the CalculateHealth delegate</summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+            damage = 0;
+        Console.WriteLine($"{name} takes {damage} damage!");
+    }
+    /// <summary>HealDamage method that follow the prototype of the CalculateHealth delegate</summary>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+            heal = 0;
+        Console.WriteLine($"{name} heals {heal} HP!");
+    }
+}
